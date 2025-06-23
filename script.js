70071c45fae8dcaf56129541076dfbfecef0c25e
lucas paqueta <132918162+luacaspaqueta@users.noreply.github.com>
2025-06-23 11:33:12 +0000

    .

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
-    {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+const perguntas = [:O que a preguiça pode causar a longo prazo?",
         alternativas: [
+    {
+enunciado
             {
                 texto: "Isso é assustador!",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
